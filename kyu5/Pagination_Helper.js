a0cf63fe407423f51ecad873f50e945cf55e6b13
sentence <snnt@protonmail.ch>
2023-01-05 21:33:24 +0100

    PaginationHelper

---
+++ kyu5/Pagination_Helper.js
@@ -0,0 +1,38 @@
+class PaginationHelper {
+  collection;
+  itemsPerPage;
+
+  constructor(collection, itemsPerPage) {
+    this.collection = collection;
+    this.itemsPerPage = itemsPerPage;
+  }
+
+  itemCount() {
+    return this.collection.length;
+  }
+
+  pageCount() {
+    return Math.ceil(this.collection.length / this.itemsPerPage);
+  }
+
+  pageItemCount(pageIndex) {
+    if (!this.hasItemAtIndex(pageIndex, this.pageCount())) return -1;
+    return (
+      this.itemsPerPage -
+      Math.ceil(
+        (((pageIndex + 1) * this.itemsPerPage) % this.itemCount()) %
+          this.itemsPerPage
+      )
+    );
+  }
+
+  pageIndex(itemIndex) {
+    if (!this.hasItemAtIndex(itemIndex, this.itemCount())) return -1;
+    return Math.ceil((itemIndex + 1) / this.itemsPerPage) - 1;
+  }
+
+  //utility
+  hasItemAtIndex(index, count) {
+    return index >= 0 && index <= count - 1;
+  }
+}
